a75c944e4489edc605d0b0bae30e6ba301178419
RaulNica <130596861+RaulNica@users.noreply.github.com>
2024-01-27 13:14:50 -0300

    Create posiciona_frota.py

---
+++ posiciona_frota.py
@@ -0,0 +1,19 @@
+def posiciona_frota(frota):
+    tabuleiro = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    
+
+    for navio in frota:
+        for posicao in navio["posicoes"]:
+            linha, coluna = posicao
+            tabuleiro[linha][coluna] = 1
+
+    return tabuleiro
